ec15c8034cc873ba4296a248446dda16062aad44
Dalton <n7dalton@gmail.com>
2022-10-08 21:50:30 -0500

    robo dude

--- Assets/Scripts/MainMenuPlayer.cs
+++ Assets/Scripts/MainMenuPlayer.cs
@@ -14,6 +14,6 @@ public class MainMenuPlayer : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        transform.Translate(Vector3.right * Time.deltaTime * speed);
+        transform.Translate(Vector3.forward * Time.deltaTime * speed);
     }
 }
